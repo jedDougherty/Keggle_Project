093c9e2b0b1e77305c16f74e3bb1cdbeb616010b
jedDougherty <jeddougherty@Jeds-MacBook-Pro.local>
2012-11-08 16:13:15 -0500

    sub_two commit
    attempted to normalize grade and counts

--- random_forest.R
+++ random_forest.R
@@ -6,42 +6,70 @@ require("plyr")
 train <- read.delim("~/Keggle_Project/train.tsv")
 test <- read.delim("~/Keggle_Project/test.tsv")
 
-#Counts Number of words in Training Data
+#Counts Number of words in Training Data and adds column to training df
 counts <- sapply(gregexpr("\\W+", train[,3]), length) + 1
+train <- cbind(train,counts)
+
+#counts number of words in testing data and adds column to testing df
 test_counts <- sapply(gregexpr("\\W+", test[,3]), length) + 1
-
-maximums <- aggregate(train$grade,list(train$set),max)
-maximums[4,2]=3
-names(maximums)<-c("set","max")
-#normalizes training data
-train_norm <- ddply(train,.(set),transform,grade = grade / max(na.omit(grade)))
-
-#Appends the normalized grade and the number of words to the set
-train_norm <- cbind(train_norm,counts)
 test <- cbind(test,test_counts)
 test$grades=0
-names(train_norm)
-training_set <- train_norm[,c(2,6,7)]
 
-testing_set <- as.data.frame(test[,c(2,4,5)])
+maximums <- aggregate(train$grade,list(train$set),max)
+minimums <- aggregate(train$grade,list(train$set),min)
+maximums[4,2]=3
+minimums[4,2]=0
+names(maximums)<-c("set","max")
+#normalizes training data
+train_norm <- ddply(train,.(set),transform,grade = (grade-min(na.omit(grade))) / (max(na.omit(grade))-min(na.omit(grade))))
+train_norm <- ddply(train_norm,.(set),transform,counts = (counts-min(na.omit(counts))) / (max(na.omit(counts))-min(na.omit(counts))))
+
+#normalizes testing wordcounts
+test <- ddply(test,.(set),transform,test_counts = (test_counts-min(na.omit(test_counts))) / (max(na.omit(test_counts))-min(na.omit(test_counts))))
+
+#Appends the normalized grade and the number of words to the set
+
+training_set <- train_norm[,c(2,6,7)]
+testing_set <- test[,c(2,4,5)]
 names(testing_set)<-c("set","counts","grade")
 
 #Regression with randomForest
 set.seed(131)
-grades.rf <- randomForest(grade ~., data = training_set,
-                              importance=TRUE, na.action=na.omit)
+grades.rf <- randomForest(grade ~ ., data = training_set,
+                              importance=TRUE, na.action=na.omit, ntree = 1000)
 estimates <-(predict(grades.rf,testing_set))
 
 train_estimates <-(predict(grades.rf,training_set))
+
+
+plot(training_set$grade,train_estimates)
+plot(estimates)
 #de-normalize estimates
-estimates <-  as.data.frame(cbind(estimates,test[,2]))
-estimates$denorm <- 0
-names(estimates) <- c("est","set")
-for (i in seq(nrow(estimates))){
-    mult <- which(maximums$set == estimates[i,2])
-    estimates[i,3] <- round(estimates[i,1]*maximums[mult,2])
+
+
+estim <-  as.data.frame(cbind(estimates,testing_set[,1]))
+estim$denorm <- 0
+names(estim) <- c("est","set","denorm")
+    for (i in seq(nrow(estim))){
+        mult <- which(maximums$set == estim[i,2])
+        estim[i,3] <- round(estim[i,1]*(maximums[mult,2]-minimums[mult,2])+minimums[mult,2])
+    }
+
+write.table(estim,file="randomForestTest.txt")
+
+
+
+##################Data Investigation
+
+#function given a data.frame with columns set,grade,counts
+#returns the plot of grade v counts for that set.
+#Put data.frame into group. put a number into the set_num 
+#variable
+sets <- function(group,set_num){
+  a <- group[group$set==set_num,]
+  b <- plot(a$grade,a$counts)
+  return(b)
 }
-write.table(estimates,file="randomForestTest.txt")
 
 
 
